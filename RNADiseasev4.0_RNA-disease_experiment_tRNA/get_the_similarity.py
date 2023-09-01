86b8cc36380a6d2ad598324f31679ba361de039b
ZYPssss <118905792+ZYPssss@users.noreply.github.com>
2023-09-01 17:31:02 +0800

    Add files via upload

--- RNADiseasev4.0_RNA-disease_experiment_tRNA/get_the_similarity.py
+++ RNADiseasev4.0_RNA-disease_experiment_tRNA/get_the_similarity.py
@@ -4,7 +4,16 @@ disease_gip_similarity = np.loadtxt('./disease_gip_similarity.csv',delimiter=','
 trna_gip_similarity = np.loadtxt('./trna_gip_similarity.csv',delimiter=',',dtype=float)
 disease_semantic_similarity = np.load('./disease_semantic_similarity.npy')
 trna_sequence_similarity = np.load('./trna_sequence_similarity.npy')
-disease_similarity = (disease_gip_similarity + disease_semantic_similarity)/2
-trna_similarity = (trna_sequence_similarity + trna_gip_similarity)/2
+
+disease_similarity = disease_semantic_similarity
+trna_similarity = trna_gip_similarity
+for i in range(len(trna_similarity)):
+    for j in range(len(trna_similarity[i])):
+        if(trna_similarity[i][j]==0):
+            trna_similarity[i][j] = trna_gip_similarity[i][j]
+for i in range(len(disease_similarity)):
+    for j in range(len(disease_similarity[i])):
+        if(disease_similarity[i][j] == 0):
+            disease_similarity[i][j] = disease_gip_similarity[i][j]
 np.savetxt('./trna_similarity.csv',trna_similarity,delimiter=',')
 np.savetxt('./disease_similarity.csv',disease_similarity,delimiter=',')
